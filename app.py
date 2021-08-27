cac279f850ae6a942dbfe1d4cd8d2ad1a690acf9
PongDev <pongwiwat.dev@outlook.com>
2021-08-27 11:27:44 +0700

    Path Parameters, Query Parameters, Request Body

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from dotenv import load_dotenv
 import os
 
@@ -13,6 +13,21 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "Hello, Flask"
+
+
+@app.route("/path_parameter/<int:id>")  # /blogs/{id}
+def pathParameter(id):
+    return f"From Path Parameter\nRequest For ID: {id}"
+
+
+@app.route("/query_parameter")  # /blogs?id=1
+def queryParameter():
+    return f"From Query Parameter\nRequest For ID: {request.args.get('id')}"
+
+
+@app.route("/request_body")
+def requestBody():
+    return f"From Request Body\nRequest For ID: {request.json['id']}"
 
 
 if __name__ == "__main__":
